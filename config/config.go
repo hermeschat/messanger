a3997496c3df16dfdd974d105bfa914b86fd9965
amirrezaask <raskarpour@gmail.com>
2019-05-13 11:12:04 +0430

    config

--- config/config.go
+++ config/config.go
@@ -7,12 +7,17 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var _config *Config
+
 //Config ...
 type Config struct {
 }
 
 //ReadConfig reads config file using given path
 func ReadConfig(filePath string) (*Config, error) {
+	if _config != nil {
+		return _config, nil
+	}
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, errors.Wrap(err, "Could not read config file")
@@ -22,5 +27,7 @@ func ReadConfig(filePath string) (*Config, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "could not unmarshall config file")
 	}
+	_config = config
+
 	return config, nil
 }
