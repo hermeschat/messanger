188f44224260dfbec234cbbe97c3621e079f6803
amirrezaask <raskarpour@gmail.com>
2020-07-29 03:23:15 +0430

    add sqlboiler models

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
-//go:generate sqlboiler --wipe psql -p repository
+//go:generate sqlboiler --wipe psql
 import (
 	"github.com/hermeschat/engine/cmd"
 	"github.com/hermeschat/engine/config"
